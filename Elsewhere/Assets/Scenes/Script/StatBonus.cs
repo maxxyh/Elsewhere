d875b610e8aba8b86ddeec5c9a804361b51b42b3
lettuceman <manle.9a4@gmail.com>
2020-05-20 02:36:01 +0800

    project re-created
    - bug: pivot/collider got problem
    - one character 
    - map

---
+++ Elsewhere/Assets/Scenes/Script/StatBonus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class StatBonus 
+{
+    public int BonusValue { get; set; }
+
+    public StatBonus(int bonusValue) 
+    {
+        this.BonusValue = bonusValue;
+        Debug.Log("A new stat bonus!");
+    }
+}
