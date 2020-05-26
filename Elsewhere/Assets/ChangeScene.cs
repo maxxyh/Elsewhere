bae9aaf8b835caf5a629cdb20011c654386ba9be
lettuceman <manle.9a4@gmail.com>
2020-05-26 14:38:06 +0800

    Added menuscreen scene (no animation)

---
+++ Elsewhere/Assets/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    public void OnlickChangeSceneButton(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+}
--- Elsewhere/Assets/Scripts/Just testing stuff/PlayerUnit.cs
+++ Elsewhere/Assets/Scripts/Just testing stuff/PlayerUnit.cs
@@ -5,17 +5,43 @@ using UnityEngine.UI;
 
 public class PlayerUnit : Unit
 {
+    private void OnDrawGizmosSelected()
+    {
+        if (this == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(this.transform.position, 0.8f);
+    }
+
     EnemyUnit enemyStats;
     public void SelectTarget() {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, 10000)) { 
-            if (hit.transform.tag == "enemy") {
-                selectedUnit = hit.transform.gameObject;
-                enemyStats = selectedUnit.transform.gameObject.transform.GetComponent<EnemyUnit>();
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null) {
+                Debug.Log(hit.collider.gameObject.tag);
+                if (hit.collider.tag == "enemy") {
+                    selectedUnit = hit.collider.gameObject;
+                    enemyStats = selectedUnit.transform.gameObject.transform.GetComponent<EnemyUnit>();
+                }
             }
         }
+
+        
+        // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // RaycastHit hit;
+        // if (Physics2D.Raycast(ray, out hit, 10000)) { 
+        //     if (hit.transform.tag == "enemy") {
+        //         selectedUnit = hit.transform.gameObject;
+        //         enemyStats = selectedUnit.transform.gameObject.transform.GetComponent<EnemyUnit>();
+        //     }
+        // }
+//         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+ 
+// if(hit.collider != null)
+// {
+//     Debug.Log ("Target Position: " + hit.collider.gameObject.transform.position);
+// }
     }
 
     void BasicAttack() {
