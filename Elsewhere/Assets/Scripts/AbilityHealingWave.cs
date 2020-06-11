e1acf02d688e88f02fd84dcb38407a0c959b8b23
Maxx <maxxyh@gmail.com>
2020-06-12 03:51:24 +0800

    Resolving merge conflicts

---
+++ Elsewhere/Assets/Scripts/AbilityHealingWave.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AbilityHealingWave : Ability
+{
+    public AbilityHealingWave() : base("Healing Wave", 3, 4, true, TargetingStyle.SINGLE)
+    {
+    }
+
+    public override IEnumerator Execute(List<Unit> targets)
+    {
+        foreach(Unit target in targets)
+        {
+            target.stats["HP"].AddModifier(new StatModifier(5, StatModType.Flat));
+            DamagePopUp.Create(target.transform.position, 5);
+        }
+
+        yield break;
+    }
+}
