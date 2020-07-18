0220215e45cd3050b7ccf4e31559a328f1ef16b3
lettuceman <manle.9a4@gmail.com>
2020-07-19 00:03:14 +0800

    Added more sprites for Items +  more stuff for Rowan
    - Edited unitConfigSave.json for Rowan stas
    - Edited abilityConfig,json for Rowan 2 (tentative) abilities
    - Added AbilityDeadlyRicochet.cs for Rowan
    - Edited detailed stats panel for Rowan (can check if u wna change his discription)
    - Added sprites for more items

---
+++ Elsewhere/Assets/Scripts/Abilities/AbilityDeadlyRicochet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class AbilityDeadlyRicochet : Ability
+{
+    public AbilityDeadlyRicochet() : base("Deadly Ricochet", 4, 3, false,
+        TargetingStyle.MULTI, new AbilityType[] {AbilityType.DAMAGE}, 2, 1)
+    {
+    }
+    
+    // Ranged AoE of 2-tile radius. Deals x1.5 physical damage to all targets in range
+    public override IEnumerator Execute(Unit initiator, List<Unit> targets)
+    {
+        foreach (Unit target in targets)
+        {
+            int attackDamage = BattleManager.CalculateMagicDamage(1.5f * initiator.stats[StatString.PHYSICAL_DAMAGE].Value, target);
+            target.stats[StatString.HP].AddModifier(new StatModifier(-attackDamage, StatModType.Flat));
+            DamagePopUp.Create(target.transform.position, string.Format("- {0} HP", attackDamage), PopupType.DAMAGE);
+        }
+        UpdateStats(initiator, targets);
+
+        yield break;
+    }
+}
